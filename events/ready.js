2920f116cbe4609cc0ad696be2a7dbaae9a8a30d
SokakFaresi <37938260+SokakFaresi@users.noreply.github.com>
2018-12-15 01:27:03 +0330

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -12,4 +12,5 @@ module.exports = client => {
   client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Created By AliEfe;
 };
